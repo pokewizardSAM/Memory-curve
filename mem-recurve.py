2f1f6c62d008bf670baa0e22b9bb93c6d082b002
pokewizardSAM <master.bloodysameer@gmail.com>
2023-12-25 22:42:12 +0530

    FIRST COMMIT FOR MEMORY RECURVE , CREATED MAIN MYSQL INTEGRATIONS AND IMPORTED FUNCTIONS FROM PROXYMAKER AS SPF

---
+++ mem-recurve.py
@@ -0,0 +1,50 @@
+import mysql.connector as mysql
+import time
+import os
+import subprocess
+from tabulate import tabulate
+import special_functions as spf
+
+conn_obj = mysql.connect(host="localhost",
+                         user="root",
+                         password="Alibha98"
+                         )
+
+if conn_obj.is_connected():
+    print("Connecting to Database..........")
+cur = conn_obj.cursor()
+cur.execute(r"CREATE DATABASE IF NOT EXISTS MEM_RECURVE;")
+cur.execute(r"USE MEM_RECURVE;")
+print("Connected to Database successfully")
+
+print("""                  Welcome to mem-recurve """)
+
+
+def date():
+    cur.execute("SELECT CURDATE();")
+    date = cur.fetchone()
+    print(f"Date: {date[0]}")
+
+def show_all_db():
+    spf.refresh_screen()
+    date()
+    cur.execute("SHOW DATABASES;")
+    return spf.centre_table(cur.fetchall(),"grid")
+
+def upadte_recurve_db():
+    cur.execute("""
+        USE MEM_RECURVE;
+        CREATE TABLE IF NOT EXISTS RECURVE_TABLE01 (
+            CHAPTER_ID INT NOT NULL PRIMARY KEY UNIQUE,
+            SUBJECT VARCHAR(10) NOT NULL,
+            CHAPTER_NAME VARCHAR(50) NOT NULL,
+            LAST_STUDY_DATE DATE NOT NULL,
+            STREAK INT NOT NULL,
+            STUDY_SCORE INT NOT NULL
+        );
+        """)
+
+while True:
+    print(spf.centre_table(['Controls'],type))
+    print(spf.centre_table(["UPDATE RECORD", "PLOT MEM RECORDS", "SAVE MEM RECORDS", "TIXE" ], type))
+    time.sleep(10)
